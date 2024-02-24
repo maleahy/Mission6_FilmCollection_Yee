b65013533ccbbf7cf05278aaaa5c9644671bbaf9
maleahy <maleahyee@gmail.com>
2024-02-23 22:11:26 -0700

    Mission 7

--- Mission6_FilmCollection_Yee/Controllers/HomeController.cs
+++ Mission6_FilmCollection_Yee/Controllers/HomeController.cs
@@ -49,5 +49,37 @@ namespace Mission6_FilmCollection_Yee.Controllers
             return View(movies); // Pass the list of movies to the view
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.Movies
+                .Single(x => x.MovieId == id);
+            ViewBag.Categories = _context.Categories.ToList();
+            return View("JoelHiltonMovieCollection", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(JoelHiltonMovieCollection updatedMovie)
+        {
+            _context.Update(updatedMovie);
+            _context.SaveChanges();
+            return RedirectToAction("table");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Movies
+                .Single(x => x.MovieId == id);
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(JoelHiltonMovieCollection updatedMovie)
+        {
+            _context.Remove(updatedMovie);
+            _context.SaveChanges();
+            return RedirectToAction("table");
+        }
     }
 }
